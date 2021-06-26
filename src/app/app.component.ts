c9bd0723af629d97015d65768120dd140a5734a4
Gabriel10Velloso <gavelloso@gmail.com>
2021-06-26 16:33:35 -0300

    testando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'appmaster';
+export class AppComponent implements OnInit{
+ 
+  
+  ngOnInit(): void {
+    const script = document.createElement('script');
+    script.src = 'http://localhost:8080/extrato/main-es2015.js';
+    document.body.appendChild(script);
+
+    const script2 = document.createElement('script');
+    script.src = 'http://localhost:8080/pagamento/main-es2015.js';
+    document.body.appendChild(script2);
+  }
 }
